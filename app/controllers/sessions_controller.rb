df9473e73eeb2c886308483ad1a95ab5a275dead
Hiroaki Matsubara <natsutteatsuiyone@gmail.com>
2011-02-18 06:23:46 +0900

    css等修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,22 @@
 class SessionsController < BaseController
   layout "login"
   
+  def create
+    @user_session = UserSession.new(:login => params[:login], :password => params[:password], :remember_me => params[:remember_me])
+
+    if @user_session.save
+
+      current_user = @user_session.record #if current_user has been called before this, it will ne nil, so we have to make to reset it
+      
+      flash[:notice] = :thanks_youre_now_logged_in.l
+      redirect_back_or_default("/")
+    else
+      flash[:notice] = :uh_oh_we_couldnt_log_you_in_with_the_username_and_password_you_entered_try_again.l
+      redirect_to teaser_path and return if AppConfig.closed_beta_mode        
+      render :action => :new
+    end
+  end
+  
   def destroy
     current_user_session.destroy
     flash[:notice] = :youve_been_logged_out_hope_you_come_back_soon.l
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+if (typeof Prototype == 'object') {
 var sidebarMenu;
-document.observe("dom:loaded", function () {
+  document.observe("dom:loaded", function () {
     sidebarMenu = new SDMenu("menu");
     sidebarMenu.init();
     sidebarMenu.speed = 4;
     sidebarMenu.remember = true;
-});
-
+  });
+}
